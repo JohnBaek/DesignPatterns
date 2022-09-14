56ea4e23c2aedc385a4b04c7f6ad2f5a0d3d9eaa
JohnMothani <87628593+JohnMothani@users.noreply.github.com>
2022-09-14 12:56:42 +0900

    - Add Solve

--- Factory_01/Interfaces/IFurnitureFactory.cs
+++ Factory_01/Interfaces/IFurnitureFactory.cs
@@ -6,8 +6,20 @@ namespace Factory_01.Interfaces;
 public interface IFurnitureFactory
 {
     /// <summary>
-    /// Returns IChair 
+    /// Returns Chair 
     /// </summary>
     /// <returns></returns>
     IChair CreateChair();
+
+    /// <summary>
+    /// Returns CoffeeTable 
+    /// </summary>
+    /// <returns></returns>
+    ICoffeeTable CreateCoffeeTable();
+    
+    /// <summary>
+    /// Returns Sofa
+    /// </summary>
+    /// <returns></returns>
+    ISofa CreateSofa();
 }
---
+++ GradingStudent/Program.cs
@@ -0,0 +1,71 @@
+﻿
+/// <summary>
+/// https://www.hackerrank.com/challenges/grading/problem?isFullScreen=true
+/// </summary>
+class Program
+{
+    public static void Main(string[] args)
+    {
+        List<int> values = new List<int>()
+        {
+            73
+            , 67
+            , 38
+            , 33
+        };
+
+        List<int> result = GradingStudents(values);
+        
+        foreach (var i in result)
+        {
+            Console.WriteLine(i);
+        }
+    }   
+    
+    /// <summary>
+    /// Returns Grades of Students
+    /// </summary>
+    /// <param name="grades"></param>
+    /// <returns></returns>
+    static List<int> GradingStudents(List<int> grades)
+    {
+        List<int> results = new ();
+        
+        // Proccess all grades
+        foreach(int grade in grades)
+        {
+            int rounded = 0;
+
+            if (grade < 38)
+            {
+                results.Add(grade);
+                continue;
+            }
+            
+            // next multiple of 5
+            if ((grade % 5) == 0)
+            {
+                rounded = grade;
+            }
+            else
+            {
+                int multiple = (grade / 5) + 1;
+                rounded = 5 * multiple;
+            }
+
+            int difference = rounded - grade;
+            
+            // less than 3
+            if(difference < 3) {
+                results.Add(rounded);
+            }
+            else {
+                results.Add(grade);
+            }
+        }
+        
+        return results;
+    }
+    
+}
+
